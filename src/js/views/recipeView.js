0c2e564d0e907723e4955e4eacc63361a6f7b9dd
ronok420 <shahariarronok41.com>
2024-08-21 21:41:29 +0600

    npm fraction added

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 
 //'url:../../img/icons.svg';
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 
 class recipeView{
@@ -89,7 +90,7 @@ class recipeView{
                   <svg class="recipe__icon">
                     <use href="${icons}#icon-check"></use>
                   </svg>
-                  <div class="recipe__quantity">${ing.quantity}</div>
+                  <div class="recipe__quantity">${new Fraction(ing.quantity).toString()}</div>
                   <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
                     ${ing.description}
